3ffc39c9987b3b89453ff71acfaedcb859754c8d
Bernardo Marques <bernardomp.dev@gmail.com>
2022-10-07 21:56:52 -0300

    Fundamentals module finished, creation of the front-end module

---
+++ Front-End/s1-JavaScript-e-Testes Assíncronos/d1-JavaScript Assíncrono-e-Callbacks/exercicios/exercicios.test.js
@@ -0,0 +1,10 @@
+// const uppercase = require("./exercicios");
+
+// describe('Testando funcao uppercase', () => {
+
+// beforeEach(() => {uppercase('abecedario', console.log)});
+
+//   test('teste 1', () => {
+//     expect(uppercase('abecedario', console.log)).toBe('ABECEDARIO') 
+//   })
+// });
