aea04c8d870de9c220800feadcac43ec936b56e4
Bernardo Marques <bernardomp.dev@gmail.com>
2022-10-18 19:29:05 -0300

    new dir created s2-intro-ao-react.

---
+++ Front-End/s1-JavaScript-e-Testes-Assíncronos/d2-Fetch-API-async-await/para-fixar/fetch.js
@@ -0,0 +1,34 @@
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+// }
+
+// fetchJoke();
+
+// // Algo deu errado :( 
+// // TypeError: Only absolute URLs are supported
+
+// Async e Await
+// O async é bastante útil! Para começar a usá-lo, basta colocar o async antes da definição de uma função. 
+// Vamos refatorar a função anterior para usar async:
+
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
