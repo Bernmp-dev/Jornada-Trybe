cb9fbef6386b1e96fe3f1cd2fab62ec64c6191e0
Bernardo Marques <bernardomp.dev@gmail.com>
2022-10-19 13:07:34 -0300

    s2-d1 fixando aprendizados realizado

---
+++ Front-End/s2-Introducao-ao-react/d1-Hello-World-no-React/exercise-my-app/src/about.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          {'Bernardo Marques'}
+        </h1>
+        <p>
+          {'Tenho 27 anos'}
+        </p>
+        <h2>
+          <ul>
+            <li> habilidade 1 </li>
+            <li> habilidade 2 </li>
+            <li> habilidade 3 </li>
+          </ul>
+        </h2>
+      </div>
+    )
+  }
+};
+
+export default About;
