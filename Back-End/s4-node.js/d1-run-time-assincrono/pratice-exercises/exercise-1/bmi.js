d913ff1c8fa4e8e5a62740972d5f2f4766bf4fd4
Bernardo Marques <bernardomp.dev@gmail.com>
2023-03-24 21:15:39 -0300

    back-end s4d1 exercise 5 complete;

--- Back-End/s4-node.js/d1-run-time-assincrono/pratice-exercises/exercise-1/bmi.js
+++ Back-End/s4-node.js/d1-run-time-assincrono/pratice-exercises/exercise-1/bmi.js
@@ -17,4 +17,20 @@ const altura = questionInt("Qual a sua altura? ");
 
 const imc = calcularIMC(peso, altura);
 
+let categoria;
+if (imc < 18.5) {
+  categoria = 'Abaixo do peso (magreza)';
+} if (imc < 25) {
+  categoria = 'Peso normal';
+} if (imc < 30) {
+  categoria = 'Acima do peso (sobrepeso)';
+} if (imc < 35) {
+  categoria = 'Obesidade grau I';
+} if (imc < 40) {
+  categoria = 'Obesidade grau II';
+} else {
+  categoria = 'Obesidade graus III e IV';
+}
+
 console.log(`Seu IMC é: ${imc}`);
+console.log(`Sua categoria de peso é: ${categoria}`);
